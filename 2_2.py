cabf25b44c089d3fc9781ffc68a3d02631d15ed4
antolow <98319496+antolow@users.noreply.github.com>
2022-06-16 23:29:00 +0500

    dz2

---
+++ 2_2.py
@@ -0,0 +1,7 @@
+print('a b c')
+for a in range (2):
+    for b in range (2):
+        for c in range (2):
+            if (a and not(c)) or (not(a) and b and c):
+                 print(a, b, c)
+            
---
+++ 2_4.py
@@ -0,0 +1,7 @@
+print('x y z')
+for x in range (2):
+    for y in range (2):
+        for z in range (2):
+            if (not(x) or y or z) and (not(x) or not(y) or z) and (x or not(y) or not(z)):
+                 print(x, y, z)
+            
